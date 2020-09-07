6900f6c0f8cd33c48be7896e647228ba9a105566
ErikBoesen <me@erikboesen.com>
2020-09-07 13:44:58 -0600

    Clean references to group_id

--- analytics.py
+++ analytics.py
@@ -9,7 +9,9 @@ class Group:
     frequency = {}
 
     def __init__(self, group_id, token):
-        raw = requests.get(f"https://api.groupme.com/v3/groups/{group_id}?token={token}").json()["response"]
+        self.group_id = group_id
+
+        raw = requests.get(f"https://api.groupme.com/v3/groups/{self.group_id}?token={token}").json()["response"]
         # Display info to user before the analysis begins
         self.message_count = raw["messages"]["count"]
         print("Analyzing " + str(self.message_count) + " messages.")
@@ -41,7 +43,7 @@ class Group:
             }
             if message_id:
                 params["before_id"] = message_id
-            response = requests.get(f"https://api.groupme.com/v3/groups/{group_id}/messages?token={self.ACCESS_TOKEN}", params=params)
+            response = requests.get(f"https://api.groupme.com/v3/groups/{self.group_id}/messages?token={self.ACCESS_TOKEN}", params=params)
             messages = response.json()["response"]["messages"]
             for message in messages:
                 message_number += 1
